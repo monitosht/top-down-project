65b3df64e0f6529fa338dd14371b641d7873d586
moni-thaker <93917467+monitosht@users.noreply.github.com>
2021-11-09 02:20:01 +1300

    Player state machine code I

--- Assets/Scripts/Player/Data/PlayerData.cs
+++ Assets/Scripts/Player/Data/PlayerData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerState : MonoBehaviour
+public class PlayerData : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
