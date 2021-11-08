63f84523eb9b549c83b13a9330701929fe0a11a4
moni-thaker <93917467+monitosht@users.noreply.github.com>
2021-11-09 02:09:17 +1300

    Completed input system setup

--- Assets/Scripts/Player/Input/PlayerInputHandler.cs
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerInputHandler : MonoBehaviour
-{
+{   
+    private Vector2 movementInput;
     public void OnMoveInput(InputAction.CallbackContext context)
     {
-
+        movementInput = context.ReadValue<Vector2>();
+        Debug.Log(movementInput);
     }
 
     public void OnJumpInput(InputAction.CallbackContext context)
     {
-
+        if(context.started)
+        {
+            Debug.Log("jump pressed");
+        }
+        if(context.performed)
+        {
+            Debug.Log("jump held");
+        }
+        if(context.canceled)
+        {
+            Debug.Log("jump released");
+        }        
     }
 }
