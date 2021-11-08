1361e7969025d5d884022e0a5c4bcab12de6964a
moni-thaker <93917467+monitosht@users.noreply.github.com>
2021-11-09 01:17:35 +1300

    Starting to script

---
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+
+    }
+}
