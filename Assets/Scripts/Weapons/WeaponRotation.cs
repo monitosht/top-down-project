c59056ba2eb6f01d4417227abebc63f0d2c89cf3
Moni <93917467+monitosht@users.noreply.github.com>
2021-11-16 04:47:04 +1300

    Completed weapon rotation

---
+++ Assets/Scripts/Weapons/WeaponRotation.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WeaponRotation : MonoBehaviour
+{
+    Vector3 mousePosition;
+    Vector3 mouseVector;
+    PlayerInput playerInput;
+
+    public Transform aim;
+
+    void Start()
+    {
+        playerInput = FindObjectOfType<PlayerInput>();
+    }
+    void Update()
+    {
+        GetMouseInput();
+        WeaponAnimation();
+    }
+
+    void GetMouseInput()
+    {
+        
+        if(playerInput.currentControlScheme == "Gamepad")
+        {
+            mouseVector = (playerInput.actions["Aim"].ReadValue<Vector2>()).normalized;
+            //mousePosition = ((playerInput.actions["Aim"].ReadValue<Vector2>()*5) + (Vector2)GameObject.FindGameObjectWithTag("Player").transform.position).normalized;
+        }
+        else
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(playerInput.actions["Aim"].ReadValue<Vector2>());
+            mouseVector = (mousePosition - transform.position).normalized;
+        }
+        
+    }
+    void WeaponAnimation()
+    {
+        float weaponAngle = Mathf.Atan2(mouseVector.y, mouseVector.x) * Mathf.Rad2Deg;
+        aim.eulerAngles = new Vector3(0, 0, weaponAngle);
+    }
+}
